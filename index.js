9acf1791c1ada96e8cf3be68735b54eba5159504
Sanjay Paul Charles <sanjaypaulcharles98@gmail.com>
2020-04-17 04:58:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // add solution here
+function theBeatlesPlay(musicians,instruments){
+  var arr;
+  for(let i=0;i<musicians.length;i++){
+    arr[i]=`${musicians[i]} plays ${instruments[i]}`
+  }
+  return(arr)
+}

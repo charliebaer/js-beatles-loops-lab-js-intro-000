43dd5af0ee99bc7bff37dd2b759ebb53b96fca4f
Sanjay Paul Charles <sanjaypaulcharles98@gmail.com>
2020-04-17 05:06:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 function theBeatlesPlay(musicians,instruments){
   var arr=[];
   for(let i=0;i<musicians.length;i++){
-    arr[i]=`"${musicians[i]} plays ${instruments[i]}"`
+    arr[i]=`${musicians[i]} plays ${instruments[i]}`
   }
   return(arr)
 }
